676802bca383ec769ffaff6d66a693391e106959
YoungGyun <noxium@naver.com>
2022-01-07 00:13:51 +0900

    study

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -81,6 +83,16 @@ public class RequestParamController {
 
     @ResponseBody
     @RequestMapping("/model-attribute-v1")
-    public String modelAtrributeV1(@Requ)
+    public String modelAtrributeV1(@ModelAttribute HelloData helloData) {
+        log.info("usernam0e={}, age={}", helloData.getUsername(), helloData.getAge());
+        return  "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAtrributeV2(HelloData helloData) {
+        log.info("usernam0e={}, age={}", helloData.getUsername(), helloData.getAge());
+        return  "ok";
+    }
 
 }

01219bc24f4c1e29d6b8aef12a6f82ec53327117
YoungGyun <noxium@naver.com>
2022-03-08 00:10:32 +0900

    Add

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Parameter;
 import java.util.Map;
 
 @Slf4j
 @Controller
 public class RequestParamController {
 
-    @ResponseBody
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
@@ -29,7 +29,7 @@ public class RequestParamController {
 
     @ResponseBody
     @RequestMapping("/request-param-v2")
-    public String requestParamV2(
+    public  String requestParamV2(
             @RequestParam("username") String memberName,
             @RequestParam("age") int memberAge) {
 
@@ -37,63 +37,81 @@ public class RequestParamController {
         return "ok";
     }
 
-    @ResponseBody
-    @RequestMapping("/request-param-v3")
-    public String requestParamV3(
-            @RequestParam String username,
-            @RequestParam int age) {
 
-        log.info("username={}, age={}", username, age);
-        return "ok";
-    }
+        @ResponseBody
+        @RequestMapping("/request-param-v3")
+        public String requestParamV3(
+                @RequestParam String username,
+                 @RequestParam int age) {
 
-    @ResponseBody
-    @RequestMapping("/request-param-v4")
-    public String requestParamV4(String username, int age) {
-        log.info("username={}, age={}", username, age);
-        return "ok";
-    }
+            log.info("username={}, age={}", username, age);
+            return "ok";
+        }
 
-    @ResponseBody
-    @RequestMapping("/request-param-required")
-    public String requestParamRequired(
-            @RequestParam(required = true) String username,
-            @RequestParam(required = false) Integer age) {
 
-        log.info("username={}, age={}", username, age);
-        return "ok";
-    }
 
-    @ResponseBody
-    @RequestMapping("/request-param-default")
-    public String requestParamDefault(
-            @RequestParam(defaultValue = "guest") String username,
-            @RequestParam(defaultValue = "-1") int age) {
 
-        log.info("username={}, age={}", username, age);
-        return "ok";
-    }
+
+        @ResponseBody
+        @RequestMapping("/request-param-v4")
+        public String requestParamV4(String username, int age) {
+
+            log.info("username={}, age={}", username, age);
+            return "ok";
+        }
+
+
+        @ResponseBody
+        @RequestMapping("/request-param-required")
+        public String requestParamRequired(
+                @RequestParam(required = true) String username,
+                @RequestParam(required = false) Integer age) {
+
+            log.info("username={}, age={}", username, age);
+            return "ok";
+        }
+
+
+        @ResponseBody
+        @RequestMapping("/request-param-default")
+        public String requestParamDefault(
+                @RequestParam(required = true, defaultValue = "guest") String username,
+                @RequestParam(required = false, defaultValue = "-1") Integer age) {
+
+            log.info("username={}, age={}", username, age);
+            return "ok";
+        }
+
 
     @ResponseBody
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
-
-        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        log.info("username={}, age={}", paramMap.get("username"),
+                paramMap.get("age"));
         return "ok";
     }
 
+
     @ResponseBody
     @RequestMapping("/model-attribute-v1")
-    public String modelAtrributeV1(@ModelAttribute HelloData helloData) {
-        log.info("usernam0e={}, age={}", helloData.getUsername(), helloData.getAge());
-        return  "ok";
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
     }
+
 
     @ResponseBody
     @RequestMapping("/model-attribute-v2")
-    public String modelAtrributeV2(HelloData helloData) {
-        log.info("usernam0e={}, age={}", helloData.getUsername(), helloData.getAge());
-        return  "ok";
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
     }
+
+
+
+
+
 
 }
